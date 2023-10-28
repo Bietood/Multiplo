1e1f6daf9c0366b7fc6415a8981ff247a75770d8
Bietood <gabriel.duarte69@etec.sp.gov.br>
2023-10-28 20:39:41 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int n1;
+int n2;
+
+Console.Write("Digite um número: ");
+int.TryParse(Console.ReadLine()!, out n1);
+Console.WriteLine();
+
+Console.Write("Avaliar se ele é múltiplo de: ");
+int.TryParse(Console.ReadLine()!, out n2);
+Console.WriteLine();
+
+if( n1 % n2 == 0)
+{
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor  = ConsoleColor.Green;
+    Console.WriteLine($"{n1} é múltiplo de {n2} ");
+    Console.ResetColor();
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{n1} não é múltiplo de {n2} ");
+    Console.ResetColor();
+}
+
